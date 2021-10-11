2552c946a34577a4b9c8b50658ebb8dbb3809de7
jhshin29 <shinji03264@gmail.com>
2021-10-11 23:45:54 +0900

    [Add] Board, Comments, Notice DTO

---
+++ src/main/java/kr/pe/midtermproject/model/domain/Notice.java
@@ -0,0 +1,45 @@
+package kr.pe.midtermproject.model.domain;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+
+@Entity
+@SequenceGenerator(name = "NOTICE_SEQ_GEN", sequenceName = "BOARD_SEQ", initialValue = 1, allocationSize = 1)
+public class Notice {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "NOTICE_SEQ_GEN")
+	private Long id;
+	
+	@Column(nullable = false)
+	private String title;
+	
+	@Column(nullable = false)
+	private String content;
+	
+	@Temporal(TemporalType.DATE)
+	@CreationTimestamp
+	@Column(nullable = false)
+	private Date created;
+}
