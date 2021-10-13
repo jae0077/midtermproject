0d6caabf9a65ce4fb929484a9552134270b37371
jhshin29 <shinji03264@gmail.com>
2021-10-13 11:18:49 +0900

    [Fix] Post 수정

--- src/main/java/kr/pe/midtermproject/controller/Controller.java
+++ src/main/java/kr/pe/midtermproject/controller/Controller.java
@@ -1,19 +1,25 @@
 package kr.pe.midtermproject.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.pe.midtermproject.model.BoardService;
 import kr.pe.midtermproject.model.TicketService;
 import kr.pe.midtermproject.model.UsersService;
+import kr.pe.midtermproject.model.domain.Board;
 import kr.pe.midtermproject.model.domain.Users;
+import kr.pe.midtermproject.model.dto.BoardDTO;
 
 @RestController
 public class Controller {
@@ -23,6 +29,9 @@ public class Controller {
 	
 	@Autowired
 	private TicketService ticketService;
+	
+	@Autowired
+	private BoardService boardService;
 
 	//회원가입
 	@PostMapping("join")
@@ -60,5 +69,29 @@ public class Controller {
 		boolean result = userService.delete(userId);
 		System.out.println(result);
 	}
+	
+	//post 작성
+	@PostMapping("board")
+	public Board createPost(@RequestBody BoardDTO board) {		
+		
+		return boardService.createBoard(board);
+	}
+	
+	@PutMapping("board/{id}")
+	public Long updatePost(@PathVariable Long id, @RequestBody BoardDTO board) {
+		return boardService.updateBoard(id, board);
+	}
+	
+//	 //개별 조회
+//    @GetMapping("/board/{id}")
+//    public BoardDTO searchById(@PathVariable Long id) {
+//        return boardService.searchById(id);
+//    }
+//    
+//    //전체 조회(목록)
+//    @GetMapping("/board")
+//    public List<BoardDTO> searchAllDesc() {
+//        return boardService.searchAllDesc();
+//    }
 
 }
