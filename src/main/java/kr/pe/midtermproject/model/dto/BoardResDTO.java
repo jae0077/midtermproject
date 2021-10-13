8707a73d8cc321edcbf7f6560fa4ab73de0d59c4
jhshin29 <shinji03264@gmail.com>
2021-10-13 15:40:48 +0900

    [Add] Read Post/List & Delete Post

---
+++ src/main/java/kr/pe/midtermproject/model/dto/BoardResDTO.java
@@ -0,0 +1,33 @@
+package kr.pe.midtermproject.model.dto;
+
+import java.util.Date;
+
+import kr.pe.midtermproject.model.domain.Board;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class BoardResDTO {
+	private Long id;
+    private String writer;
+    private String title;
+    private String content;
+    private Date created;
+    private Date updated;
+    
+    public BoardResDTO(Board entity) {
+        this.id = entity.getBoardIdx();
+        this.writer = entity.getWriter().getName();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.created = entity.getCreated();
+        this.updated = entity.getUpdated();
+    }
+}
