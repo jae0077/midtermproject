fe5472e7148e1d00dddd34aa7c9587c8f1769516
jh <sjj0077@naver.com>
2021-10-15 11:24:38 +0900

    [Fix] verifyUserId

--- src/main/java/kr/pe/midtermproject/dao/NoticeRepository.java
+++ src/main/java/kr/pe/midtermproject/dao/NoticeRepository.java
@@ -3,10 +3,11 @@ package kr.pe.midtermproject.dao;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import kr.pe.midtermproject.model.domain.Notice;
 
-public interface NoticeRepository extends CrudRepository<Notice, Long>{
+public interface NoticeRepository extends CrudRepository<Notice, Long>, PagingAndSortingRepository<Notice, Long>{
 
 	// 공지사항 역순 정렬
 	List<Notice> findAllByOrderByNoticeIdxDesc();
